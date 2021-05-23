e5002eda63aae678d3b242eff76d5ae11e3c9c8a
vpanameno <valerie.michelle@icloud.com>
2021-05-22 18:36:53 -0700

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ function asyncHandler(cb) {
   return async (req, res, next) => {
     try {
       await cb(req, res, next);
-    } catch (error) 
+    } catch (error) {
       next(error);
     }
   };
